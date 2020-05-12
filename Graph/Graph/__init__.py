51d49cbed208f02a2bc53c1278ec04ded71ead4e
Basudeb Roy <54861078+Basudeb96@users.noreply.github.com>
2020-05-13 01:10:30 +0530

    Update __init__.py

--- Graph/Graph/__init__.py
+++ Graph/Graph/__init__.py
@@ -1,17 +1,1 @@
-from setuptools import setup
-
-setup(name = "Graph_project",
-    version = "0.0.4",
-    author = "Basudeb Roy",
-    author_email = "basudebroy82@gmail.com",
-    description = ("An demonstration of Creating graph."),
-    license = "BSD",
-    keywords = "Graph tutorial",
-    url = "http://packages.python.org/an_example_pypi_project",
-    packages=['an_example_pypi_project', 'tests'],
-    long_description=read('README'),
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],)
+import Graph as Graph
